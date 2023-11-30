f3eeff21bc9f094a58e1d5d33a8f24a42fff1ca2
migzy27 <152022120+migzy27@users.noreply.github.com>
2023-11-30 10:12:02 +0800

    Add files via upload

--- src/staff/MovieDescription.java
+++ src/staff/MovieDescription.java
@@ -1,7 +1,86 @@
 package staff;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class MovieDescription extends JFrame {
     
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    JLabel mDescription = new JLabel("MOVIE DETAILS");
+    JLabel title = new JLabel("Title:");
+    JLabel director = new JLabel("Director:");
+    JLabel duration = new JLabel("Duration:");
+    JLabel genre = new JLabel("Genre:");
+    
+    MovieDescription() {
+        setVisible(true);
+        setSize(800, 600);
+        setResizable(false);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        
+        panel1.setLayout(null);
+        add(panel1);
+        panel1.setBackground(Color.gray);
+        panel1.setSize(new Dimension(300,600));
+        
+        panel1.add(mDescription);
+        mDescription.setFont(new Font("Courier",Font.BOLD,15));
+        mDescription.setBounds(90, 10, 150, 30);
+        
+        panel2.setLayout(null);
+        panel1.add(panel2);
+        panel2.setBackground(Color.LIGHT_GRAY);
+        panel2.setBorder(BorderFactory.createLineBorder(Color.black));
+        panel2.setSize(new Dimension(280,500));
+        panel2.setBounds(10, 50, 280, 500);
+        
+        panel2.add(title);
+        title.setBounds(55, 340, 100, 30);
+        panel2.add(director);
+        director.setBounds(55, 370, 100, 30);
+        panel2.add(duration);
+        duration.setBounds(55, 400, 100, 30);
+        panel2.add(genre);
+        genre.setBounds(55, 430, 100, 30);
+    }
+    public void movie1() {
+        ImageIcon p1 = new ImageIcon("movie1.png");
+        JLabel movie1 = new JLabel(p1);
+        panel2.add(movie1);
+        movie1.setBounds(30, 0, 220, 360);
+        
+    }
+    
+    public void movie2() {
+        ImageIcon p2 = new ImageIcon("movie2.png");
+        JLabel movie2 = new JLabel(p2);
+        panel2.add(movie2);
+        movie2.setBounds(30, 0, 220, 360);
+    }
+    public void movie3() {
+        ImageIcon p3 = new ImageIcon("movie3.png");
+        JLabel movie3 = new JLabel(p3);
+        panel2.add(movie3);
+        movie3.setBounds(30, 0, 220, 360);
+    }
+    public void movie4() {
+        ImageIcon p4 = new ImageIcon("movie4.png");
+        JLabel movie4 = new JLabel(p4);
+        panel2.add(movie4);
+        movie4.setBounds(30, 0, 220, 360);
+    }
+    public void movie5() {
+        ImageIcon p5 = new ImageIcon("movie5.png");
+        JLabel movie5 = new JLabel(p5);
+        panel2.add(movie5);
+        movie5.setBounds(30, 0, 220, 360);
+    }
+    public void movie6() {
+        ImageIcon p6 = new ImageIcon("movie6.png");
+        JLabel movie6 = new JLabel(p6);
+        panel2.add(movie6);
+        movie6.setBounds(30, 0, 220, 360);
+    }
 }
--- src/staff/Staff.java
+++ src/staff/Staff.java
@@ -6,7 +6,8 @@ import javax.swing.*;
 public class Staff extends JFrame {
     
     public void staff() {
-        setSize(900, 600);
+        setSize(900, 680);
+        setResizable(false);
         setLocationRelativeTo(null);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,10 +17,11 @@ public class Staff extends JFrame {
         movie.setLayout(null);
         add(movie);
         movie.setBackground(Color.gray);
-        movie.setSize(new Dimension(300,600));
+        movie.setSize(new Dimension(300,680));
         
         JLabel avlM = new JLabel("AVAILABLE MOVIES");
         movie.add(avlM);
+        avlM.setFont(new Font("Courier",Font.BOLD,15));
         avlM.setBounds(80, 10, 150, 30);
         
         JPanel movies = new JPanel();
@@ -27,120 +29,114 @@ public class Staff extends JFrame {
         movie.add(movies);
         movies.setBackground(Color.LIGHT_GRAY);
         movies.setBorder(BorderFactory.createLineBorder(Color.black));
-        movies.setSize(new Dimension(280,500));
-        movies.setBounds(10, 50, 280, 500);
-        JLabel movie1 = new JLabel("Gagamboy");
+        movies.setSize(new Dimension(280,580));
+        movies.setBounds(10, 50, 280, 580);
+        
+        JLabel movie1 = new JLabel("Parasite");
         movies.add(movie1);
-        ImageIcon poster1 = new ImageIcon("movie.png");
+        ImageIcon poster1 = new ImageIcon("m1.png");
         JLabel m1 = new JLabel(poster1);
         movies.add(m1);
-        m1.setBounds(20, 60, 100, 100);
-        movie1.setBounds(30, 155, 150, 30);
-        JLabel movie2 = new JLabel("Gagamboy");
+        m1.setBounds(20, 15, 100, 140);
+        movie1.setBounds(45, 155, 150, 30);
+        
+        JLabel movie2 = new JLabel("Taxi Driver");
         movies.add(movie2);
-        ImageIcon poster2 = new ImageIcon("movie2.png");
+        ImageIcon poster2 = new ImageIcon("m2.png");
         JLabel m2 = new JLabel(poster2);
         movies.add(m2);
-        m2.setBounds(160, 60, 100, 100);
-        movie2.setBounds(170, 155, 150, 30);
-        JLabel movie3 = new JLabel("Gagamboy");
+        m2.setBounds(160, 15, 100, 140);
+        movie2.setBounds(180, 155, 150, 30);
+        
+        JLabel movie3 = new JLabel("Seven");
         movies.add(movie3);
-        ImageIcon poster3 = new ImageIcon("movie3.png");
+        ImageIcon poster3 = new ImageIcon("m3.png");
         JLabel m3 = new JLabel(poster3);
         movies.add(m3);
-        m3.setBounds(20, 213, 100, 100);
-        movie3.setBounds(30, 310, 150, 30);
-        JLabel movie4 = new JLabel("Gagamboy");
+        m3.setBounds(20, 185, 100, 140);
+        movie3.setBounds(50, 325, 150, 30);
+        
+        JLabel movie4 = new JLabel("Nope");
         movies.add(movie4);
-        ImageIcon poster4 = new ImageIcon("movie4.png");
+        ImageIcon poster4 = new ImageIcon("m4.png");
         JLabel m4 = new JLabel(poster4);
         movies.add(m4);
-        m4.setBounds(160, 213, 100, 100);
-        movie4.setBounds(170, 310, 150, 30);
-        JLabel movie5 = new JLabel("Gagamboy");
+        m4.setBounds(160, 185, 100, 140);
+        movie4.setBounds(195, 325, 150, 30);
+        
+        JLabel movie5 = new JLabel("Memento");
         movies.add(movie5);
-        ImageIcon poster5 = new ImageIcon("movie5.png");
+        ImageIcon poster5 = new ImageIcon("m5.png");
         JLabel m5 = new JLabel(poster5);
         movies.add(m5);
-        m5.setBounds(20, 366, 100, 100);
-        movie5.setBounds(30, 465, 150, 30);
-        JLabel movie6 = new JLabel("Gagamboy");
+        m5.setBounds(20, 355, 100, 140);
+        movie5.setBounds(45, 495, 150, 30);
+        
+        JLabel movie6 = new JLabel("Home Alone");
         movies.add(movie6);
-        ImageIcon poster6 = new ImageIcon("movie6.png");
+        ImageIcon poster6 = new ImageIcon("m6.png");
         JLabel m6 = new JLabel(poster6);
         movies.add(m6);
-        m6.setBounds(160, 366, 100, 100);
-        movie6.setBounds(170, 465, 150, 30);
+        m6.setBounds(160, 355, 100, 140);
+        movie6.setBounds(175, 495, 150, 30);
         
         JButton logOut = new JButton("Logout");
         movie.add(logOut);
-        logOut.setBounds(75, 560, 150, 30);
+        logOut.setBounds(75, 580, 150, 30);
         
         m1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFrame frm = new JFrame();
-                frm.setVisible(true);
-                frm.setSize(350, 500);
-                frm.setLocationRelativeTo(null);
+                MovieDescription md = new MovieDescription();
+                md.movie1();
             }
         });
         
         m2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFrame frm = new JFrame();
-                frm.setVisible(true);
-                frm.setSize(350, 500);
-                frm.setLocationRelativeTo(null);
+                MovieDescription md = new MovieDescription();
+                md.movie2();
             }
         });
         
         m3.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFrame frm = new JFrame();
-                frm.setVisible(true);
-                frm.setSize(350, 500);
-                frm.setLocationRelativeTo(null);
+                MovieDescription md = new MovieDescription();
+                md.movie3();
             }
         });
         
         m4.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFrame frm = new JFrame();
-                frm.setVisible(true);
-                frm.setSize(350, 500);
-                frm.setLocationRelativeTo(null);
+                MovieDescription md = new MovieDescription();
+                md.movie4();
             }
         });
         
         m5.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFrame frm = new JFrame();
-                frm.setVisible(true);
-                frm.setSize(350, 500);
-                frm.setLocationRelativeTo(null);
+                MovieDescription md = new MovieDescription();
+                md.movie5();
             }
         });
         
         m6.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFrame frm = new JFrame();
-                frm.setVisible(true);
-                frm.setSize(350, 500);
-                frm.setLocationRelativeTo(null);
+                MovieDescription md = new MovieDescription();
+                md.movie6();
             }
         });
         
         logOut.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                if(logOut.getActionCommand().equals("Logout")){
-                    System.out.println("etits");
+                if(logOut.getActionCommand().equals("Logout")) {
+                    JOptionPane.showMessageDialog(null, "You have logged out successfully", "Logout", JOptionPane.YES_OPTION);
                     System.exit(0);
                 }
             }
